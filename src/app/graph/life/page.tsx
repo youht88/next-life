35ee2b24ec8cc5322e798756efb76722d08bc0c5
youht <youht@youhtdeMac-mini.local>
2024-09-18 16:33:02 +0800

    update

--- src/app/graph/life/page.tsx
+++ src/app/graph/life/page.tsx
@@ -1,37 +1,50 @@
 "use client"
 // pages/index.tsx
 import Head from 'next/head';
-import React, { useCallback, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+// lib/mongodb.ts
+import { Assistant, Client, Thread } from '@langchain/langgraph-sdk';
 import Chat from './chat';
 
-const Home: React.FC = () => {
-  const [scrollToBottom, setScrollToBottom] = useState(false);
-  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+const uri = "http://youht.cc:18123"; // 从环境变量中获取 MongoDB URI
+const options = {};
 
-  // 用于处理滚动到最底部
-  const handleNewMessage = useCallback(() => {
-    setScrollToBottom(true);
-  }, []);
+console.log("HERE!!!!!!!!")
 
-  // 处理滚动逻辑
-  React.useEffect(() => {
-    if (scrollToBottom && messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-      setScrollToBottom(false);
-    }
-  }, [scrollToBottom]);
-
-  return (
-    <div className="flex flex-col h-screen">
-      <Head>
-        <title>Chat with AI</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="flex-1 overflow-hidden">
-        <Chat onNewMessage={handleNewMessage} />
+export default function Home(){
+    const [scrollToBottom, setScrollToBottom] = useState(false);
+    const messagesEndRef = useRef<HTMLDivElement | null>(null);
+    const [theme, setTheme] = useState('light')
+    // 用于处理滚动到最底部
+    const handleNewMessage = useCallback(() => {
+      setScrollToBottom(true);
+    }, []);
+    const [client, setClient] = useState<Client|null>(null);
+    const [assistants, setAssistants] = useState<Assistant[]>([]);
+    const [thread, setThread] = useState<Thread|null>(null);
+    useEffect(() => {
+      const initLanggraph = async ()=>{
+        if (client) return;
+        console.log("initLanggraph....")
+        const _client = new Client({apiUrl:uri}); 
+        setClient(_client);
+        const _assistants = await _client.assistants.search();
+        setAssistants(_assistants)
+        const _thread =  await _client.threads.create();
+        setThread(_thread)
+      }
+      initLanggraph()
+    }, [])
+    
+    return (
+      <div className="flex flex-col h-screen">
+        <Head>
+          <title>Chat with AI</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className="flex-1 overflow-hidden">
+          <Chat client={client} assistants={assistants} thread={thread}  />
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Home;
+    );
+  };
